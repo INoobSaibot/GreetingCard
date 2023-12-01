360c191a9b8f3920683182df268e5f09228f4310
INoobSaibot <INoobSaibot@users.noreply.github.com>
2023-12-01 14:53:16 -0500

    Added some hacky code for use effect and detecting key press combinations.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,70 @@
 // import logo from './logo.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+let hackRanCount = 0;
 
-function App() {
+function App(){
+  useEffect(()=> {
+    if(hackRanCount > 0) {return};
+    hackRanCount++; // shouldnt need this, its a hack, idk why its running twice!
+    console.log(document)
+    document.addEventListener('keyup', (e) => {
+       handleKeyPress(e)
+      }, true)
+  }, []);
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+  }, []); // <- add empty brackets here
+
+  let keyPressesArr = ''
+  let timeOut = setTimeout(()=>{})
+  const codes = [];
+  function addCode(arr){
+    codes.push(arr.toString().replace(/,/g, ''));
+  }
+
+  let code = ['ArrowUp', 'ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'ArrowLeft']
+  addCode(code);
+  addCode('abcd')
+  code ='abcd'
+  codes.push(code)
+  const handleKeyPress = (e) => {
+    // console.log(e)
+    console.log(e.key)
+    keyPressesArr = keyPressesArr.concat(e.key)
+    clearTimeout(timeOut)
+    timeOut = setTimeout( ()=> {
+      keyPressesArr = ''
+    },1500)
+    console.log(keyPressesArr)
+    console.log(keyPressesArr.includes(code))
+    codes.forEach((code)=> {
+      if(keyPressesArr.includes(code)){
+        handleCodePressed()
+      }
+    })
+    
+  }
+
+  const handleCodePressed = () => {
+    alert('coooooodes')
+    keyPressesArr = ''
+  }
+
+  return <Greeting count={count}></Greeting>
+}
+
+function Greeting() {
   let getParamsObject = ([...(new URLSearchParams(window.location.search))]).reduce((prev,curr)=>(Object.assign(prev,{[curr[0]]:curr[1]})),{})
   let decodedString = 'default message'
-  console.log('use btoa("someMessageText"), then added it to the url like this... url?codedStringFromFunction')
-  console.log(`${window.location.origin}?m=codedStringFrom_btoaFunction`)
-  console.log(`${window.location.origin}?m=R3JlZXRpbmdz`)
+  // console.log('use btoa("someMessageText"), then added it to the url like this... url?codedStringFromFunction')
+  // console.log(`${window.location.origin}?m=codedStringFrom_btoaFunction`)
+  // console.log(`${window.location.origin}?m=R3JlZXRpbmdz`)
 
   console.log(`also now you can just type 'generateUrl('message') in the console to get a custom url made`)
   // console.log(generateUrl('yooooooooeeee')  )
